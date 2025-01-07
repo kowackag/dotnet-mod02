3d0fbc490a6d0531765e9aa9a72e257c7fc344c6
Małgorzata Kowacka <kowackag@gmail.com>
2025-01-07 20:53:00 +0100

    feat: task 01

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,7 +6,33 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tasks modul 02");
+            Console.WriteLine();
+
+            Console.WriteLine("-----Task 1-----");
+            DisplayPersonalData();
+
+            Console.WriteLine();
+            Console.WriteLine("-----Task 2-----");
         }
+
+        static void DisplayPersonalData()
+        {
+
+            string name = "Gosia";
+            string surname = "Kot";
+            int age = 44;
+            char gender = 'k';
+            string personalId = "12345678901";
+            string personalNumber = "AB1234567";
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Surname: {surname}");
+            Console.WriteLine($"Age: {age} years");
+            Console.WriteLine($"Gender: {gender}");
+            Console.WriteLine($"Personal Id: {personalId}");
+            Console.WriteLine($"Personal number: {personalNumber}");
+        }
+
     }
 }
