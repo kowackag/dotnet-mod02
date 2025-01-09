ac7ddb082595e174df1955490e7837d6198cfe50
Małgorzata Kowacka <kowackag@gmail.com>
2025-01-09 19:03:57 +0100

    feat: task 03

---
+++ FirstApp/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    internal class Class1
+    {
+        public int num {  get; set; }
+        public string name { get; set; }
+    }
+}
--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FirstApp;
 
 namespace MyApp
 {
@@ -17,9 +18,9 @@ namespace MyApp
             DisplaySomeChars();
 
             Console.WriteLine();
-            Console.WriteLine("-----Task 3-----"); 
+            Console.WriteLine("-----Task 3-----");
+            CountDiagonal();
         }
-
         static void DisplayPersonalData()
         {
 
@@ -45,6 +46,41 @@ namespace MyApp
             char c = 'c';
             Console.WriteLine($"Chars in reversed order: {c}, {b}, {a}");
 
+        }
+
+        static void CountDiagonal()
+        {
+            Console.WriteLine("Enter the length of the rectangle: ");
+            double length = 0;
+            string? lengthString = Console.ReadLine();
+            bool isCorrectLength = double.TryParse(lengthString, out length);
+            while (!isCorrectLength)
+            {
+                Console.WriteLine("Incorrect value. Please enter the correct length of the rectangle: ");
+                lengthString = Console.ReadLine();
+                isCorrectLength = double.TryParse(lengthString, out length);
+            }
+
+            Console.WriteLine("Enter the height of the rectangle: ");
+            double height = 0;
+            string? heightString = Console.ReadLine();
+            bool isCorrectHeight = double.TryParse(heightString, out height);
+            while (!isCorrectHeight)
+            {
+                Console.WriteLine("Incorrect value. Please enter the correct height of the rectangle: ");
+                heightString = Console.ReadLine();
+                isCorrectHeight = double.TryParse(heightString, out height);
+            }
+
+            if (isCorrectLength && isCorrectHeight)
+            {
+                double diagonal = Math.Sqrt(Math.Pow(height, 2) + Math.Pow(length, 2));
+                Console.WriteLine($"The length of the diagonal is: {diagonal}");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect value. Task was ended");
+            }
         }
     }
 }
