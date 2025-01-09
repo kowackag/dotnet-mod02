968400f433000b9d7710f48b09277f7d80e6c233
Małgorzata Kowacka <kowackag@gmail.com>
2025-01-09 22:57:42 +0100

    feat:task 05

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -23,6 +23,10 @@ namespace MyApp
             Console.WriteLine();
             Console.WriteLine("-----Task 4-----");
             DisplayVariablesInfo();
+
+            Console.WriteLine();
+            Console.WriteLine("-----Task 5-----");
+            GetPersoalData();
         }
         static void DisplayPersonalData()
         {
@@ -94,6 +98,72 @@ namespace MyApp
             Console.WriteLine($"Number: {number} - Type {number.GetType()}");
             Console.WriteLine($"Title: {title} - Type {title.GetType()}");
             Console.WriteLine($"FloatNumber: {floatNumber} - Type {floatNumber.GetType()}");
+        }
+
+        static void GetPersoalData()
+        {
+            Console.WriteLine("Enter your name: ");
+            string? nameString = Console.ReadLine();
+            while (string.IsNullOrEmpty(nameString))
+            {
+                Console.WriteLine("Incorrect name. Try again");
+                nameString = Console.ReadLine();
+            }
+
+            Console.WriteLine("Enter your surname: ");
+            string? surnameString = Console.ReadLine();
+            while (string.IsNullOrEmpty(surnameString))
+            {
+                Console.WriteLine("Incorrect surname. Try again");
+                surnameString = Console.ReadLine();
+            }
+
+            Console.WriteLine("Enter your phone number - only number ");
+            string? phoneNumber = Console.ReadLine();
+            bool isCorrectPhoneNumber = long.TryParse(phoneNumber, out long phoneNumberLong) && phoneNumber.Length == 9;
+            while (!isCorrectPhoneNumber)
+            {
+                Console.WriteLine("Incorrect phone number. Try again");
+                phoneNumber = Console.ReadLine();
+                isCorrectPhoneNumber = long.TryParse(phoneNumber, out phoneNumberLong) && phoneNumber.Length == 9;
+            }
+
+            Console.WriteLine("Enter your email");
+            string? email = Console.ReadLine();
+            bool isCorrectEmail = !string.IsNullOrEmpty(email) && email.Contains("@") && email.Contains(".");
+           
+            while (!isCorrectEmail)
+            {
+                Console.WriteLine("Incorrect email. Try again");
+                email = Console.ReadLine();
+                isCorrectEmail = !string.IsNullOrEmpty(email) && email.Contains("@") && email.Contains(".");
+            }
+
+            Console.WriteLine("Enter your height in cm");
+            string? heightString = Console.ReadLine();
+            int height = 0;
+            bool isCorrectHeight = int.TryParse(heightString, out height) && height > 80 && height <250;
+
+            while (!isCorrectHeight)
+            {
+                Console.WriteLine("Incorrect height. Try again");
+                heightString = Console.ReadLine();
+                isCorrectHeight = int.TryParse(heightString, out height);
+            }
+
+            Console.WriteLine("Enter your weight in kg");
+            string? weightString = Console.ReadLine();
+            double weight = 0;
+            bool isCorrectWeight = double.TryParse(weightString, out weight) && weight > 20 && weight < 280;
+
+            while (!isCorrectWeight)
+            {
+                Console.WriteLine("Incorrect weight. Try again");
+                weightString = Console.ReadLine();
+                isCorrectWeight = double.TryParse(weightString, out weight);
+            }
+
+            Console.WriteLine($"That was all questions. {nameString} {surnameString} Thank you for your response"); 
         }
     }
 }
