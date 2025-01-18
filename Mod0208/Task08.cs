1fe15197a77917ca656d9906bef6f2231d0fc9fb
Małgorzata Kowacka <kowackag@gmail.com>
2025-01-18 16:53:41 +0100

    feat: finish tasks mod02 lesson 08

--- Mod0208/Task08.cs
+++ Mod0208/Task08.cs
@@ -5,12 +5,14 @@ using static System.Formats.Asn1.AsnWriter;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 namespace MyApp
 {
     internal class Task07Mod02
     {
         static void Main(string[] args)
         {
+            Task10();
             Console.WriteLine("Tasks modul 02 lesson 08");
             Console.WriteLine();
             Console.WriteLine("-----Task 1-----");
@@ -42,15 +44,15 @@ namespace MyApp
 
             Console.WriteLine();
             Console.WriteLine("-----Task 8-----");
-          
+            Task08();
 
             Console.WriteLine();
             Console.WriteLine("-----Task 9-----");
-          
+            Task09();
 
             Console.WriteLine();
             Console.WriteLine("-----Task 10-----");
-                 
+
         }
         static void Task01()
         {
@@ -64,7 +66,7 @@ namespace MyApp
                 }
                 if (n == 2) primeNumbersAmmount++;
             }
-            Console.WriteLine($"Prime numbers is: {primeNumbersAmmount}"); 
+            Console.WriteLine($"Prime numbers is: {primeNumbersAmmount}");
         }
 
         static void Task02()
@@ -73,8 +75,8 @@ namespace MyApp
             List<string> list = new List<string>();
             do
             {
-                if (i%2==0) list.Add(i.ToString());
-                i++; 
+                if (i % 2 == 0) list.Add(i.ToString());
+                i++;
             } while (i <= 1000);
 
             Console.WriteLine($"The given number is: {string.Join(',', list)}");
@@ -85,20 +87,21 @@ namespace MyApp
             int last = 1;
             int prev = 0;
             string score = "1";
-            List<int> chain =new List<int>();
+            List<int> chain = new List<int>();
 
-         
-            while (last <= 200000000) {
+
+            while (last <= 200000000)
+            {
                 int copyLast = last;
                 score = score + last.ToString();
                 chain.Add(copyLast);
                 last = last + prev;
                 prev = copyLast;
             }
- 
+
             Console.WriteLine("chain:" + string.Join(',', chain));
-        }   
-       
+        }
+
         static void Task04()
         {
             Console.WriteLine("Enter integer number a:");
@@ -107,7 +110,7 @@ namespace MyApp
             int print = 1;
             for (int i = 1; print <= max; i++)
             {
-                for (int j=1;j<=i;j++)
+                for (int j = 1; j <= i; j++)
                 {
                     if (print <= max)
                     {
@@ -115,7 +118,7 @@ namespace MyApp
                         print++;
                     }
                 }
-                Console.WriteLine();     
+                Console.WriteLine();
             }
         }
 
@@ -130,9 +133,9 @@ namespace MyApp
         static void Task06()
         {
             float sum = 0;
-            for (int i=1; i <= 20; i++)
+            for (int i = 1; i <= 20; i++)
             {
-                sum = sum + (1f/i);
+                sum = sum + (1f / i);
             }
             Console.WriteLine($"The score is: {sum}");
         }
@@ -140,9 +143,9 @@ namespace MyApp
         static void Task07()
         {
             int number = 13;
-            for (int i = 1; i <= number; i+=2)
+            for (int i = 1; i <= number; i += 2)
             {
-                int emptyNo = (number - i)/2;
+                int emptyNo = (number - i) / 2;
                 for (int j = 1; j <= emptyNo; j++)
                 {
                     Console.Write(" ");
@@ -157,7 +160,7 @@ namespace MyApp
                 }
                 Console.WriteLine("");
             }
-            for (int i = number-2; i >= 1; i -= 2)
+            for (int i = number - 2; i >= 1; i -= 2)
             {
                 int emptyNo = (number - i) / 2;
                 for (int j = 1; j <= emptyNo; j++)
@@ -176,225 +179,52 @@ namespace MyApp
             }
         }
 
-        static void CheckIfCanBeAuthoritive()
+        static void Task08()
         {
-            Console.WriteLine("Give me your years");
-            int years = GetIntegerNumber();
-            Console.WriteLine($"You can be authoritive: {years >= 21}");
-        }
-        static void CheckHeight()
-        {
-            Console.WriteLine("Enter your height in cm");
-            int height = GetIntegerNumber();
-            if (height < 140)
+            Console.WriteLine("Type some string");
+            string? chain = Console.ReadLine();
+            if (chain != null)
             {
-                Console.WriteLine("You are dwarf");
-            }
-            else if (height >= 140 && height < 160)
-            {
-                Console.WriteLine("You are short");
-            }
-            else if (height >= 160 && height < 180)
-            {
-                Console.WriteLine("You are quite tall");
+                Console.WriteLine($"Reverse: {string.Join("", chain.ToCharArray().Reverse())}");
             }
             else
             {
-                Console.WriteLine("You are tall");
+                Console.WriteLine("");
             }
         }
-
-        static void DisplayTheBigestNumber()
+        static void Task09()
         {
-            Console.WriteLine("Enter integer number a:");
-            int a = GetIntegerNumber();
+            Console.WriteLine("Enter some integer");
+            int number = GetIntegerNumber();
+            List<int> integerNumbers = new List<int>();
 
-            Console.WriteLine("Enter integer number b:");
-            int b = GetIntegerNumber();
+            for (int i = number; i >= 1; i = i / 2)
+            {
+                integerNumbers.Add(i % 2);
+            }
 
-            Console.WriteLine("Enter integer number c:");
-            int c = GetIntegerNumber();
-
-            List<int> listNumbers = new List<int>() { a, b, c };
-            listNumbers.Sort();
-
-            Console.WriteLine($"The Bigest number is: {listNumbers[2]}");
+            integerNumbers.Reverse();
+            if (integerNumbers.Count > 0) Console.WriteLine("Binary: " + string.Join("", integerNumbers));
         }
 
-        static void DisplayIfCanBeTheStudent()
+        static void Task10()
         {
-            Console.WriteLine("Enter your score of Math");
-            int math = GetIntegerNumber();
-            Console.WriteLine("Enter your score of Physics");
-            int physics = GetIntegerNumber();
-            Console.WriteLine("Enter your score of Chemist");
-            int chemist = GetIntegerNumber();
+            Console.WriteLine("Enter some integer");
+            int numberA = GetIntegerNumber();
 
-            bool canBeTheStudent = false;
-            if (math > 70 && physics > 55 && chemist > 45 && (math + chemist + physics) > 180)
-            {
-                canBeTheStudent = true;
-            }
+            Console.WriteLine("Enter some integer");
+            int numberB = GetIntegerNumber();
 
-            if (math + physics > 150 || math + chemist > 150)
+            int num=0;
+            for (int i = 1; i <=numberA*numberB; i++)
             {
-                canBeTheStudent = true;
-            }
-
-            if (canBeTheStudent)
-            {
-                Console.WriteLine("You can be a student");
-            }
-            else
-            {
-                Console.WriteLine("You can not be a student. Try again next year");
-            }
-        }
-
-        static void DisplayIfCanBuildTriangle()
-        {
-            Console.WriteLine("Enter the first dimension");
-            int dimention01 = GetIntegerNumber();
-            Console.WriteLine("Enter the second dimension");
-            int dimention02 = GetIntegerNumber();
-            Console.WriteLine("Enter the third dimension");
-            int dimention03 = GetIntegerNumber();
-
-            int[] intArray = new int[] { dimention01, dimention02, dimention03 };
-
-            Array.Sort(intArray);
-            if (intArray[0] + intArray[1] > intArray[2])
-            {
-                Console.WriteLine("You can build triangle");
-            }
-            else
-            {
-                Console.WriteLine("You can not build triangle");
-            }
-        }
-
-        static void CheckTheWeather()
-        {
-            Console.WriteLine("Enter the temperature in full Celsius");
-            int temp = GetIntegerNumber();
-
-            if (temp < 0)
-            {
-                Console.WriteLine("cholernie piździ");
-            }
-            else if (temp >= 0 && temp < 10)
-            {
-                Console.WriteLine("zimno");
-            }
-            else if (temp >= 10 && temp < 20)
-            {
-                Console.WriteLine("chłodno");
-            }
-            else if (temp >= 20 && temp < 30)
-            {
-                Console.WriteLine("w sam raz");
-            }
-            else if (temp >= 30 && temp < 40)
-            {
-                Console.WriteLine("zaczyna być słabo, bo gorąco");
-            }
-            else
-            {
-                Console.WriteLine("a weź wyprowadzam się na Alaskę.");
-            }
-        }
-        static void DisplayScore()
-        {
-            Console.WriteLine("Enter the score 1-6");
-            string? numberString = Console.ReadLine();
-            bool isCorrectNumber = int.TryParse(numberString, out int number) && number > 0 && number < 7;
-            while (!isCorrectNumber)
-            {
-                Console.WriteLine("Incorrect number. Please enter the correct integer number: ");
-                numberString = Console.ReadLine();
-                isCorrectNumber = int.TryParse(numberString, out number) && number > 0 && number < 7;
-            }
-            string score;
-            switch (number)
-            {
-                case 1:
-                    score = "niedostateczny";
+                if ((i  % numberB) == 0 && (i % numberA == 0))
+                {
+                    num = i;
                     break;
-                case 2:
-                    score = "dopuszczający";
-                    break;
-                case 3:
-                    score = "dostateczny";
-                    break;
-                case 4:
-                    score = "dobry";
-                    break;
-                case 5:
-                    score = "bardzo dobry";
-                    break;
-                case 6:
-                    score = "celujący";
-                    break;
-                default:
-                    score = "unknown";
-                    break;
+                }
             }
-            Console.WriteLine($"Your score in polish is: {score}");
-        }
-
-        static void DisplayMathScore()
-        {
-            Console.WriteLine("Enter the number A:");
-            string? firstNumberString = Console.ReadLine();
-            bool isCorrectNumber = double.TryParse(firstNumberString, out double firstNumber);
-            while (!isCorrectNumber)
-            {
-                Console.WriteLine("Incorrect number. Please enter the correct number: ");
-                firstNumberString = Console.ReadLine();
-                isCorrectNumber = double.TryParse(firstNumberString, out firstNumber);
-            }
-
-            Console.WriteLine("Enter the number B:");
-            string? secondNumberString = Console.ReadLine();
-            isCorrectNumber = double.TryParse(secondNumberString, out double secondNumber);
-            while (!isCorrectNumber)
-            {
-                Console.WriteLine("Incorrect number. Please enter the correct number: ");
-                firstNumberString = Console.ReadLine();
-                isCorrectNumber = double.TryParse(firstNumberString, out secondNumber);
-            }
-
-            Console.WriteLine("Type opperation: 1 => addition, 2=> substraction, 3=>multiplying, 4=> division, another else quit");
-
-            string? operation = Console.ReadLine();
-
-            switch (operation)
-            {
-                case "1":
-                    Console.WriteLine($"The score is: {firstNumber + secondNumber} ");
-                    break;
-                case "2":
-                    Console.WriteLine($"The score is: {firstNumber - secondNumber} ");
-                    break;
-                case "3":
-                    Console.WriteLine($"The score is: {firstNumber * secondNumber} ");
-                    break;
-                case "4":
-                    {
-                        if (secondNumber != 0)
-                        {
-                            Console.WriteLine($"The score is: {firstNumber / secondNumber} ");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"You can not divide by zero");
-                        }
-                        break;
-                    }
-                default:
-                    Console.WriteLine("Good by");
-                    break;
-            }
+            Console.WriteLine(num);
         }
         //helpers
 
@@ -409,6 +239,7 @@ namespace MyApp
                 isCorrectNumber = int.TryParse(numberString, out number);
             }
             return number;
+
         }
     }
 }
